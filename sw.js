9e45c33683916ff077450c49c5c034fcdd6b0a1c
EvelynBahena7u7 <100085522+EvelynBahena7u7@users.noreply.github.com>
2022-11-25 10:23:16 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@ const promiseCache= caches.open('cache-v1').then((cache)=>{
         cache.add('images/icons/android-launchericon-72-72.png');
         cache.add('images/icons/android-launchericon-96-96.png');
         cache.add('images/icons/android-launchericon-144-144.png');
-        cache.adadd('images/icons/android-launchericon-192-192.png');
-        cache.d('images/icons/android-launchericon-512-512.png');
+        cache.add('images/icons/android-launchericon-192-192.png');
+        cache.add('images/icons/android-launchericon-512-512.png');
         cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/manifest.json');
         cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
         cache.add('https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js');
