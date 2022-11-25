37a9e2fc92eafd1a9856f1cba7f4095d866627d9
EvelynBahena7u7 <100085522+EvelynBahena7u7@users.noreply.github.com>
2022-11-25 10:11:42 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,50 +1,31 @@
-const STATIC_CACHE_NAME = 'static-cache-v1.2';
-const INMUTABLE_CACHE_NAME = 'inmutable-cache-v1.1';
-const DYNAMIC_CACHE_NAME = 'dynamic-cache-v1.1';
+self.addEventListener('install',(event)=>{
+  console.log('sw: Instalado');
+const promiseCache= caches.open('cache-v1').then((cache)=>{
+        cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/');
+        cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/index.html');
+        cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/app.js');
+        cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/js/camera.js');
+        cache.add('images/icons/android-launchericon-48-48.png');
+        cache.add('images/icons/android-launchericon-72-72.png');
+        cache.add('images/icons/android-launchericon-96-96.png');
+        cache.add('images/icons/android-launchericon-144-144.png');
+        cache.add('images/icons/android-launchericon-192-192.png');
+        cache.add('images/icons/android-launchericon-512-512.png');
+        cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/manifest.json');
+        cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
+        cache.add('https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js');
+        cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js');
+      })
+      event.waitUntil(promiseCache);
+ })
 
-self.addEventListener('install', (event) => {
-    const respCache = caches.open(STATIC_CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        'https://evelynbahena7u7.github.io/PWA-Practica8/',
-        'https://evelynbahena7u7.github.io/PWA-Practica8/index.html',
-        'https://evelynbahena7u7.github.io/PWA-Practica8/app.js',
-        'https://evelynbahena7u7.github.io/PWA-Practica8/js/camera.js',
-        'images/icons/android-launchericon-48-48.png',
-        'images/icons/android-launchericon-72-72.png',
-        'images/icons/android-launchericon-96-96.png',
-        'images/icons/android-launchericon-144-144.png',
-        'images/icons/android-launchericon-192-192.png',
-        'images/icons/android-launchericon-512-512.png',
-        'https://evelynbahena7u7.github.io/PWA-Practica8/manifest.json'
-      ]);
-    });
-    const respCacheInmutable = caches.open(INMUTABLE_CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css',
-        'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js'
-      ]);
-    });
- 
-    event.waitUntil(Promise.all([respCache,respCacheInmutable]));
- 
-});
-
-self.addEventListener('activate', (event) =>{
-    console.log("Activado!");
-
-    const proDelete = caches.keys().then((cachesItems) =>{
-        cachesItems.forEach(element =>{
-            if(element !== STATIC_CACHE_NAME   && element.includes('static')){
-                return caches.delete(element);
-            }
-        })
-    })
-
-    event.waitUntil(proDelete);
+ self.addEventListener('fetch', (event)=>{
+  event.respondWith(
+      caches.match(event.request).then((response)=>{
+          if(response){
+              return response;
+          }
+          return fetch(event.request);
+      })
+  );
 })
-
-self.addEventListener('fetch', event =>{
-    const respCache = caches.match(event.request)
-    event.respondWith(respCache);
-});
