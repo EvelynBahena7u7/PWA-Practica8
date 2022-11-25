9c8a06c38e8d20d34944abee72b74f2f529548c7
EvelynBahena7u7 <100085522+EvelynBahena7u7@users.noreply.github.com>
2022-11-25 10:20:10 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,14 +3,14 @@ self.addEventListener('install',(event)=>{
 const promiseCache= caches.open('cache-v1').then((cache)=>{
         cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/');
         cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/index.html');
-        cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/app.js');
+        cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/js/app.js');
         cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/js/camera.js');
         cache.add('images/icons/android-launchericon-48-48.png');
         cache.add('images/icons/android-launchericon-72-72.png');
         cache.add('images/icons/android-launchericon-96-96.png');
         cache.add('images/icons/android-launchericon-144-144.png');
-        cache.add('images/icons/android-launchericon-192-192.png');
-        cache.add('images/icons/android-launchericon-512-512.png');
+        cache.adadd('images/icons/android-launchericon-192-192.png');
+        cache.d('images/icons/android-launchericon-512-512.png');
         cache.add('https://evelynbahena7u7.github.io/PWA-Practica8/manifest.json');
         cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
         cache.add('https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js');
